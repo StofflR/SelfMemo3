0af903789395f2d503de74e4a5f02781bf3b1137
Piotr Siewiera <piotr.siewiera@student.tugraz.at>
2026-01-06 20:06:01 +0100

    Website title changed to SelfMemo 3.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Analytics } from '@vercel/analytics/react';
 import { Toaster } from "react-hot-toast";
 
 export const metadata = {
-  title: 'SelfMemo 2.0 - Manage your reminders',
+  title: 'SelfMemo 3.0 - Manage your reminders',
   description:
     'A simple reminder app to help you manage your reminders.',
 };
